8200539fdee3471d781406b0fe732268c280dc3e
Ian Beddie <ian.beddie@mths.ca>
2024-09-25 02:59:31 +0000

    added enstein program

---
+++ index.ts
@@ -0,0 +1,29 @@
+/*
+* This is a program that calculates energy produced.
+*
+* @author  Ian Beddie
+* @version 1.0
+* @since 2024-09-24
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+const speedOfLight = 2.998 * 10 ** 8
+
+const objectMassInput = createPrompt(
+        "Enter the mass of your object in kiligrams: ")
+
+if (objectMassInput.value === null) {
+  console.log("You did not enter any input.")
+  } else {
+          const objectMassString = objectMassInput.value
+          if (isNaN(parseFloat(objectMassString))) {
+          console.log("You did not enter a number.")
+             } else {
+                     const objectMass = parseFloat(objectMassString)
+                     const energyOutput = objectMass * speedOfLight ** 2
+                     console.log(`${objectMass}kg of mass would produce ${energyOutput}J of energy. `)
+             }
+  }
+console.log("\nDone.")
+
